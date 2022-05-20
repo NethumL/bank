ef26e3564a9abeeb60234d65d6cb0c392041d622
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-20 20:23:53 +0530

    Add `InstalmentRepository`

---
+++ src/Repository/InstalmentRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class InstalmentRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function findAllByUser(string $userId): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $stmt = $conn->prepare("SELECT * FROM Installment I JOIN Loan L ON I.Loan_ID = L.ID WHERE User_ID = ?");
+        $resultSet = $stmt->executeQuery([$userId]);
+        return $resultSet->fetchAllAssociative();
+    }
+}
