594409e0c14814f5eebc19bafc70b315073a01a9
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-05-21 18:02:04 +0530

    Implement `/instalment/view` route

--- src/Controller/InstalmentController.php
+++ src/Controller/InstalmentController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\InstalmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InstalmentController extends AbstractController
 {
-    #[Route('/instalment/view', name: 'app_instalment_view')]
-    public function view(): Response
+    #[Route('/instalment/view', name: 'app_instalment_view', methods: ['GET'])]
+    public function view(InstalmentRepository $instalmentRepository): Response
     {
+        $user = $this->getUser();
+        $instalments = $instalmentRepository->findAllByUser($user->getId());
         return $this->render('instalment/view.html.twig', [
-            'controller_name' => 'InstalmentController',
+            'instalments' => $instalments,
         ]);
     }
 
