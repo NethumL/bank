56de6b40f33fbeeac956e8e8ac90c387862dbd96
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-06-04 00:10:53 +0530

    Add Symfony form for transfer

---
+++ src/Form/TransferType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Transaction;
+use App\Repository\AccountRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TransferType extends AbstractType
+{
+    private AccountRepository $accountRepository;
+
+    public function __construct(AccountRepository $accountRepository)
+    {
+        $this->accountRepository = $accountRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $accounts = $this->accountRepository->findByUser($options['userId']);
+
+        $accountChoices = array_map(function ($account) {
+            return [$account['Account_Number'] . ": " . $account['Account_Type'] => $account['Account_Number']];
+        }, $accounts);
+
+        $builder
+            ->add('from', ChoiceType::class, ['choices' => $accountChoices])
+            ->add('to', TextType::class)
+            ->add('type', TextType::class, ['disabled' => true])
+            ->add('amount', MoneyType::class, ['currency' => ''])
+            ->add('transfer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Transaction::class,
+            'userId' => null,
+        ]);
+
+        $resolver->addAllowedTypes('userId', 'string');
+    }
+}
