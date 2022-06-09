cc618e16350e800ac01b590156bde48bd3bb5cff
KalinduGandara <kalindugandara16@gmail.com>
2022-06-09 18:11:47 +0530

    add templates

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,158 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+require('bootstrap/js/dist/tooltip');
+require('bootstrap/js/dist/popover');
 
 // start the Stimulus application
-import './bootstrap';
+// import './bootstrap';
+const $ = require('jquery');
+
+const bootstrap = require('bootstrap');
+require('metismenu');
+
+const Waves = require('node-waves');
+
+require('simplebar');
+
+(function ($) {
+
+    'use strict';
+
+    function initMetisMenu() {
+        //metis menu
+        $("#side-menu").metisMenu();
+    }
+
+    function initLeftMenuCollapse() {
+        $('#vertical-menu-btn').on('click', function (event) {
+            event.preventDefault();
+            $('body').toggleClass('sidebar-enable');
+            if ($(window).width() >= 992) {
+                $('body').toggleClass('vertical-collpsed');
+            } else {
+                $('body').removeClass('vertical-collpsed');
+            }
+        });
+    }
+
+    function initActiveMenu() {
+        // === following js will activate the menu in left side bar based on url ====
+        $("#sidebar-menu a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("mm-active"); // add active to li of the current link
+                $(this).parent().parent().addClass("mm-show");
+                $(this).parent().parent().prev().addClass("mm-active"); // add active class to an anchor
+                $(this).parent().parent().parent().addClass("mm-active");
+                $(this).parent().parent().parent().parent().addClass("mm-show"); // add active to li of the current link
+                $(this).parent().parent().parent().parent().parent().addClass("mm-active");
+            }
+        });
+    }
+
+    function initMenuItem() {
+        $(".navbar-nav a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active");
+                $(this).parent().parent().addClass("active");
+                $(this).parent().parent().parent().parent().addClass("active");
+                $(this).parent().parent().parent().parent().parent().addClass("active");
+            }
+        });
+    }
+
+    function initFullScreen() {
+        $('[data-toggle="fullscreen"]').on("click", function (e) {
+            e.preventDefault();
+            $('body').toggleClass('fullscreen-enable');
+            if (!document.fullscreenElement && /* alternative standard method */ !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+                if (document.documentElement.requestFullscreen) {
+                    document.documentElement.requestFullscreen();
+                } else if (document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                } else if (document.documentElement.webkitRequestFullscreen) {
+                    document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+                }
+            } else {
+                if (document.cancelFullScreen) {
+                    document.cancelFullScreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitCancelFullScreen) {
+                    document.webkitCancelFullScreen();
+                }
+            }
+        });
+        document.addEventListener('fullscreenchange', exitHandler );
+        document.addEventListener("webkitfullscreenchange", exitHandler);
+        document.addEventListener("mozfullscreenchange", exitHandler);
+        function exitHandler() {
+            if (!document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+                console.log('pressed');
+                $('body').removeClass('fullscreen-enable');
+            }
+        }
+    }
+
+    function initRightSidebar() {
+        // right side-bar toggle
+        $('.right-bar-toggle').on('click', function (e) {
+            $('body').toggleClass('right-bar-enabled');
+        });
+
+        $(document).on('click', 'body', function (e) {
+            if ($(e.target).closest('.right-bar-toggle, .right-bar').length > 0) {
+                return;
+            }
+
+            $('body').removeClass('right-bar-enabled');
+            return;
+        });
+    }
+
+    function initDropdownMenu() {
+        $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+            console.log('asd');
+            if (!$(this).next().hasClass('show')) {
+                $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+            }
+            var $subMenu = $(this).next(".dropdown-menu");
+            $subMenu.toggleClass('show');
+
+            return false;
+        });
+    }
+
+    function initComponents() {
+        const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        });
+        const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+        const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl,null)
+        });
+    }
+
+    function init() {
+        initMetisMenu();
+        initLeftMenuCollapse();
+        initActiveMenu();
+        initMenuItem();
+        initFullScreen();
+        initRightSidebar();
+        initDropdownMenu();
+        initComponents();
+        Waves.init();
+    }
+
+    init();
+
+})($)
+
