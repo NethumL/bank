80230e3c32e1601806f430487606bb15c685b1aa
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-06-18 00:18:43 +0530

    Add CSRF protection to login form

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -36,7 +37,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     {
         $username = $request->request->get("_username");
         $credentials = new PasswordCredentials($request->request->get("_password"));
-        return new Passport(new UserBadge($username), $credentials);
+        $csrfToken = $request->request->get("_csrf_token");
+        return new Passport(new UserBadge($username), $credentials, [new CsrfTokenBadge('login', $csrfToken)]);
     }
 
     /**
