c6b10ed7fb6dc3897f1fd9868af27f8cb78f3443
Nimantha Cooray <nimanthacooray@gmail.com>
2022-06-22 08:43:32 +0530

    Add sample data sql file

---
+++ sample-data.sql
@@ -0,0 +1,35 @@
+INSERT INTO `user`
+VALUES (
+           '22c4fd6a-96d4-47f3-9cec-590ac79a9155',
+           'nimantha',
+           '$2y$10$U6heiACk37qCtIo4kuUcC.zU.vCOVku.ILVcbrEr7M7VeguXE36D2',
+           'Nimantha Cooray',
+           'MANAGER',
+           '0775316852',
+           '1999-11-10',
+           '172, Chilaw Road, Negombo'
+       );
+
+INSERT INTO `user`
+VALUES (
+           'd160bf33-4a85-43b4-b713-ae846653b611',
+           'nethum',
+           '$2y$10$FYLCmxJxEU9B.uivnr37yuzMGMnk5/RIeEGWCXnG.yBNZM6KC.5TS',
+           'Nethum Lamahewage',
+           'EMPLOYEE',
+           '0775365895',
+           '1999-11-10',
+           '122, Ragama Road, Ja Ela'
+       );
+
+INSERT INTO `user`
+VALUES (
+           'ccdf4450-e455-4c4e-b899-85ad8d8fd4e2',
+           'bhanuja',
+           '$2y$10$4pNFGVBLFQSHTiqgbK1YhOymTa/JR61M2wxZOHOscW13vUyU7Dwze',
+           'Bhanuja Sasanka',
+           'CUSTOMER',
+           '0778956235',
+           '1999-05-10',
+           '56, Thodu Road, Kurana'
+       );
