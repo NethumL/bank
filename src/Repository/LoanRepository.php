a786e9663544fb2cfb9f5d319820aeb568f04500
Nimantha Cooray <nimanthacooray@gmail.com>
2022-06-22 08:41:48 +0530

    Add `findAllByUser` to LoanRepository

---
+++ src/Repository/LoanRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Loan;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Uid\Uuid;
+
+class LoanRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Loan::class);
+    }
+
+    public function findAllByUser(string $userId): array|bool
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $stmt = $conn->prepare("SELECT * FROM Loan L WHERE L.User_ID = ?");
+        $resultSet = $stmt->executeQuery([$userId]);
+        return $resultSet->fetchAllAssociative();
+    }
+}
