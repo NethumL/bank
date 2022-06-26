a710e6f689f1b50f64e35dce3d4b1c3bab6e8ff4
Nimantha Cooray <nimanthacooray@gmail.com>
2022-06-26 11:12:58 +0530

    extend `OnlineLoan` from `Loan`

--- src/Entity/OnlineLoan.php
+++ src/Entity/OnlineLoan.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="Online_Loan", indexes={@ORM\Index(name="Online_Loan", columns={"ID"})})
  * @ORM\Entity
  */
-class OnlineLoan
+class OnlineLoan extends Loan
 {
     private $fdId;
 
