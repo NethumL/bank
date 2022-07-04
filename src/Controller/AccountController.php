d4fc7287c99a27a00e2ee7350fa80866f1a4f805
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-07-04 22:50:36 +0530

    Remove `/account/edit` route

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,12 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\BranchType;
-use App\Form\EditUserType;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -34,28 +29,6 @@ class AccountController extends AbstractController
     {
         return $this->render('account/view.html.twig', [
             'controller_name' => 'AccountController',
-        ]);
-    }
-
-    #[Route('/account/edit', name: 'app_account_edit')]
-    public function edit(Request $request, UserRepository $userRepository): Response
-    {
-        /** @var User $user */
-        $user = $this->getUser();
-
-        $form = $this->createForm(EditUserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user = $form->getData();
-            $userRepository->update($user);
-
-            return $this->redirectToRoute('app_account_edit', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('account/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
         ]);
     }
 }
