4a35dd934f02230563c58a28d3bbe3cfc40b21f9
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-07-04 15:58:58 +0530

    Implement CRUD functionality for branches

--- src/Controller/BranchController.php
+++ src/Controller/BranchController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Branch;
 use App\Form\BranchType;
 use App\Repository\BranchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,27 +9,76 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/branch')]
 class BranchController extends AbstractController
 {
-    #[Route('/branch/new', name: 'app_branch_new')]
+    #[Route('/', name: 'app_branch_index', methods: ['GET'])]
+    public function index(BranchRepository $branchRepository): Response
+    {
+        $branches = $branchRepository->findAll();
+
+        return $this->render('branch/index.html.twig', [
+            'branches' => $branches,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_branch_new', methods: ['GET', 'POST'])]
     public function new(Request $request, BranchRepository $branchRepository): Response
     {
-        $branch = new Branch();
-        $form = $this->createForm(BranchType::class, $branch);
+        $form = $this->createForm(BranchType::class, []);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            /** @var Branch $branch */
+        if ($form->isSubmitted() && $form->isValid()) {
             $branch = $form->getData();
-
             $branchRepository->insert($branch);
-            return $this->redirectToRoute('app_branch_new');
+
+            return $this->redirectToRoute('app_branch_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('branch/new.html.twig', [
-            'controller_name' => 'BranchController',
-            'form' => $form
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_branch_show', methods: ['GET'])]
+    public function show(string $id, BranchRepository $branchRepository): Response
+    {
+        $branch = $branchRepository->findOneById($id);
+
+        return $this->render('branch/show.html.twig', [
+            'branch' => $branch,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_branch_edit', methods: ['GET', 'POST'])]
+    public function edit(string $id, Request $request, BranchRepository $branchRepository): Response
+    {
+        $branch = $branchRepository->findOneById($id);
+
+        $form = $this->createForm(BranchType::class, $branch);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $branch = $form->getData();
+            $branchRepository->update($branch);
+
+            return $this->redirectToRoute('app_branch_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('branch/edit.html.twig', [
+            'branch' => $branch,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_branch_delete', methods: ['POST'])]
+    public function delete(string $id, Request $request, BranchRepository $branchRepository): Response
+    {
+        $branch = $branchRepository->findOneById($id);
+        if ($this->isCsrfTokenValid('delete' . $branch['ID'], $request->request->get('_token'))) {
+            $branchRepository->delete($id);
+        }
+
+        return $this->redirectToRoute('app_branch_index', [], Response::HTTP_SEE_OTHER);
     }
 }
