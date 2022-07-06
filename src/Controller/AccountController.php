abfcbb2c2542e2d8483f98faf463b091902747d0
Bhanuja1999 <bhanusasanka1999@gmail.com>
2022-07-06 23:42:57 +0530

    account/create and account/view

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,90 +2,73 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\AccountType;
-use App\Repository\AccountRepository;
-use App\Repository\EmployeeRepository;
-use App\Repository\SavingsRepository;
-use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Repository\AccountRepository;
+use App\Repository\FdRepository;
+use App\Form\CustomerType;
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class AccountController extends AbstractController
 {
-    private EntityManagerInterface $em;
-
-    public function __construct(EntityManagerInterface $em)
-    {
-        $this->em = $em;
-    }
-
     #[Route('/account/create', name: 'app_account_create')]
-    public function create(): Response
+    public function create(
+        Request                     $request,
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository              $userRepository,
+    ): Response
+
     {
-        return $this->render('account/create.html.twig', [
+        /** @var User $user */
+        $user = $this->getUser();
+        $newUser = new User();
+        $form = $this->createForm(CustomerType::class, $newUser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $newUser */
+            $newUser = $form->getData();
+            $newUser->setUserType('CUSTOMER');
+            $hashedPassword = $passwordHasher->hashPassword($newUser, $newUser->getPassword());
+            $newUser->setPassword($hashedPassword);
+            $userRepository->insert($newUser);
+            return $this->redirectToRoute('app_account_create');
+        }
+
+        return $this->renderForm('account/create.html.twig', [
             'controller_name' => 'AccountController',
+            'form' => $form
         ]);
     }
 
     #[Route('/account/create-new', name: 'app_account_create_new')]
-    public function createNew(
-        Request            $request,
-        UserRepository     $userRepository,
-        EmployeeRepository $employeeRepository,
-        AccountRepository  $accountRepository,
-        SavingsRepository  $savingsRepository,
-    ): Response
+    public function createNew(): Response
     {
-        /** @var User $user */
-        $user = $this->getUser();
-        $employee = $employeeRepository->findOneById($user->getId());
-
-        $form = $this->createForm(AccountType::class, []);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var array $account */
-            $account = $form->getData();
-            $account['branchId'] = $employee['Branch_ID'];
-
-            $customer = $userRepository->findOneByUsername($account['username']);
-            $account['userId'] = $customer->getId();
-
-            $conn = $this->em->getConnection();
-            $conn->beginTransaction();
-
-            try {
-                $accountNumber = $accountRepository->insert($account);
-                if ($account['accountType'] === 'SAVINGS') {
-                    $savingsRepository->insert($accountNumber, $account['savingsPlan']);
-                }
-
-                $this->addFlash("new-account-number", "New account number is " . $accountNumber . ".");
-
-                $conn->commit();
-            } catch (Exception $e) {
-                $conn->rollBack();
-                throw $e;
-            }
-
-            return $this->redirectToRoute("app_account_create_new");
-        }
-
-        return $this->renderForm('account/create_new.html.twig', [
-            'form' => $form,
+        return $this->render('account/create_new.html.twig', [
+            'controller_name' => 'AccountController',
         ]);
     }
 
     #[Route('/account/view', name: 'app_account_view')]
-    public function view(): Response
+    public function view(AccountRepository $accountRepository, FdRepository $fdRepository): Response
     {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $accounts = $accountRepository->findByUser($user->getId());
+        $fds = $fdRepository->findByUser($user->getId());
+
         return $this->render('account/view.html.twig', [
             'controller_name' => 'AccountController',
+            'accounts' => $accounts,
+            'fds' => $fds
         ]);
     }
 }
--- src/Repository/AccountRepository.php
+++ src/Repository/AccountRepository.php
@@ -8,11 +8,6 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class AccountRepository extends ServiceEntityRepository
 {
-    private static array $typeToCodeMap = [
-        'SAVINGS' => '164',
-        'CURRENT' => '925'
-    ];
-
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Account::class);
@@ -21,7 +16,7 @@ class AccountRepository extends ServiceEntityRepository
     public function findByUser(string $userId, string $accountType = ""): array|bool
     {
         $conn = $this->getEntityManager()->getConnection();
-        $sql = "SELECT * FROM Account A WHERE A.User_ID = ?";
+        $sql = "SELECT A.*,Branch.Name AS Branch_Name FROM Account A JOIN Branch ON A.Branch_ID = Branch.ID WHERE A.User_ID = ?";
         $params = [$userId];
         if ($accountType) {
             $sql .= " AND A.Account_Type = ?";
@@ -45,49 +40,5 @@ class AccountRepository extends ServiceEntityRepository
         $conn = $this->getEntityManager()->getConnection();
         $stmt = $conn->prepare("UPDATE Account SET Amount = ? WHERE Account_Number = ?");
         return $stmt->executeStatement([$newAmount, $id]);
-    }
-
-    public function insert(array $account): string
-    {
-        $conn = $this->getEntityManager()->getConnection();
-
-        $accountNumber = $this->getNextAccountNumber($account);
-
-        $stmt = $conn->prepare("
-            INSERT INTO Account(Account_Number, User_ID, Branch_ID, Account_Type, Amount)
-            VALUES(?, ?, ?, ?, ?);
-        ");
-        $stmt->executeStatement([
-            $accountNumber,
-            $account['userId'],
-            $account['branchId'],
-            $account['accountType'],
-            $account['amount'],
-        ]);
-        return $accountNumber;
-    }
-
-    public function getNextAccountNumber(array $accountDetails): string
-    {
-        $conn = $this->getEntityManager()->getConnection();
-
-        $branch = $accountDetails['branchId'];
-        $fromBranch = substr(base_convert(md5($branch), 16, 10), 0, 3);
-
-        $fromType = self::$typeToCodeMap[$accountDetails['accountType']];
-
-        $stmt = $conn->prepare("SELECT Account_Number FROM Account WHERE Branch_ID = ? ORDER BY Created_Time DESC LIMIT 1");
-        $result = $stmt->executeQuery([$accountDetails['branchId']]);
-        $prevAccountNumber = $result->fetchOne();
-        if ($prevAccountNumber !== false) {
-            $prevIncremental = substr($prevAccountNumber, 6, 7);
-        } else {
-            $prevIncremental = 0;
-        }
-        $fromIncremental = str_pad(strval(intval($prevIncremental) + 1), 7, '0', STR_PAD_LEFT);
-
-        $fromRandom = strval(random_int(0, 999));
-
-        return $fromBranch . $fromType . $fromIncremental . $fromRandom;
     }
 }
